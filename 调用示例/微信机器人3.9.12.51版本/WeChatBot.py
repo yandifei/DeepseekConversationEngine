250ee18f47af010a438a2d259e89695fdc1a83ca
yandifei <3058439878@qq.com>
2025-06-16 23:38:42 +0800

    暂时确定研发方向

--- 调用示例/微信机器人3.9.12.51版本/WeChatBot.py
+++ 调用示例/微信机器人3.9.12.51版本/WeChatBot.py
@@ -4,7 +4,7 @@ from time import sleep
 from wcbo import WeChatBackgroundOperation, Chats, Contacts
 
 # wc = WeChatBackgroundOperation()
-wc = WeChatBackgroundOperation()
+wc = WeChatBackgroundOperation("","",False)
 print(f"微信名：{wc.name}\n微信ID：{wc.id}")
 
 # cts = Contacts(wc)  # 创建通讯录对象，传入整个微信客户端对象
@@ -25,7 +25,7 @@ print(f"微信名：{wc.name}\n微信ID：{wc.id}")
 # wc.back_click(wc.chats_button)  # 点击聊天按钮
 cs = Chats(wc)   # 创建聊天对象，传入整个微信客户端对象
 cs.click() # 点击聊天控件并获得会话列表
-wc.back_double_click(wc.chats_button)  # 再次点击聊天按钮(双击)定位到新消息的对话列表
+# wc.back_double_click(wc.chats_button)  # 再次点击聊天按钮(双击)定位到新消息的对话列表
 # 点击新消息的发送者并将返回值进行判断，2是点击后等待时间，True是打印检查新消息的结果
 if cs.select_new_message(2,True):
     cs.get_all_message(True)    # 有新信息就深度解析消息体，True打印解析的消息
